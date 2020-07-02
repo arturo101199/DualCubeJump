79d15c3e2fafa089a55fd737ce830331a52215d9
arturo101199 <al375559@uji.es>
2020-07-02 17:32:46 +0200

    Made Main Menu

---
+++ DualCubeJump/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    const float ROTATION_SPEED = 2f;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * ROTATION_SPEED * Time.deltaTime);
+    }
+}
