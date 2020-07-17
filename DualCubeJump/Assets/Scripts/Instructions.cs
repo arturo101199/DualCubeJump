eb1f97d95806c1f0d7280cb13f793197df04f26f
arturo101199 <al375559@uji.es>
2020-07-17 13:05:38 +0200

    Made Instructions for the game

---
+++ DualCubeJump/Assets/Scripts/Instructions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instructions : MonoBehaviour
+{
+    public GameObject[] instructions;
+    public GameObject nextButton;
+
+    int currentInstruction = 0;
+    
+    public void startInstructions()
+    {
+        instructions[currentInstruction].SetActive(true);
+        nextButton.SetActive(true);
+    }
+
+    public void passInstruction()
+    {
+        instructions[currentInstruction].SetActive(false);
+        currentInstruction++;
+        if(currentInstruction < instructions.Length)
+            instructions[currentInstruction].SetActive(true);
+        else
+        {
+            nextButton.SetActive(false);
+            currentInstruction = 0;
+        }
+    }
+}
