ac777d0e64ad35e221e78f14d11c993ad99f8693
arturo101199 <al375559@uji.es>
2020-07-21 12:20:50 +0200

    Added animation for changing background color

---
+++ DualCubeJump/Assets/Scripts/Camera/ChangeBackgroundColor.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class ChangeBackgroundColor : MonoBehaviour
+{
+    public AnimationCurve animationCurve;
+    public Color[] colors;
+
+    Camera camera;
+    Color backgroundColor;
+    Color newBackgroundColor;
+    float timer;
+    float animationDuration;
+    int prevRand = 0;
+
+
+    void Awake()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    void Start()
+    {
+        ChangeColor();
+        animationDuration = animationCurve.keys[animationCurve.length - 1].time;
+
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >= animationDuration)
+        {
+            timer = 0;
+            ChangeColor();
+        }
+        InterpolateColor();
+    }
+
+    void ChangeColor()
+    {
+        int rand = Random.Range(0, colors.Length);
+        while(rand == prevRand)
+            rand = Random.Range(0, colors.Length);
+        prevRand = rand;
+        backgroundColor = camera.backgroundColor;
+        newBackgroundColor = colors[rand];
+    }
+
+    void InterpolateColor()
+    {
+        float timeValue = animationCurve.Evaluate(timer);
+        Color interpolatedColor = Color.Lerp(backgroundColor, newBackgroundColor, timeValue);
+        camera.backgroundColor = interpolatedColor;
+        RenderSettings.fogColor = interpolatedColor;
+    }
+}
